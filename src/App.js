2d901760e84918208ecb4b59ef8fcb6c55735e8b
SanchitYadav2005 <sanchit2005yudavanshi@gmail.com>
2022-08-08 02:11:09 -0700

    done with the final changes in the style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Form from "./components/Form"
+import Navbar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Form />
     </div>
   );
