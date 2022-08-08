f6b1e3b58139d27d76754c767cbe58ef63496c46
SanchitYadav2005 <sanchit2005yudavanshi@gmail.com>
2022-08-08 02:10:15 -0700

    added my final style changes to the form

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Navbar(){
+    return(
+        <nav className="nav_bar">
+            <h1 className="nav_heading">Business Card</h1>
+        </nav>
+    )
+}
+
+export default Navbar;
